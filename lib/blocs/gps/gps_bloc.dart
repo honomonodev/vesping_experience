085cbb053058fa899cedc797e04bb53cb18cdef1
GitRockie <150247498+honomonodev@users.noreply.github.com>
2024-05-09 01:01:34 +0200

    ConfigurationAndroid&iOS
    Creating listeners to GPS Status

--- lib/blocs/gps/gps_bloc.dart
+++ lib/blocs/gps/gps_bloc.dart
@@ -1,5 +1,6 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:geolocator/geolocator.dart';
 
 part 'gps_event.dart';
 part 'gps_state.dart';
@@ -12,5 +13,29 @@ class GpsBloc extends Bloc<GpsEvent, GpsState> {
           isGpsEnabled: event.isGpsEnabled,
           isGpsPermissionGranted: event.isGpsPermissionGranted,
         )));
+
+    _init();
+  }
+
+  Future<void> _init() async {
+    final isEnabled = await _checkGpsStatus();
+    print('isEnable: $isEnabled');
+  }
+
+  Future<bool> _checkGpsStatus() async {
+    final isEnable = Geolocator.isLocationServiceEnabled();
+
+    Geolocator.getServiceStatusStream().listen((event) {
+      final isEnabled = (event.index == 1) ? true : false;
+      print('service status: $isEnabled ');
+    });
+
+    return isEnable;
+  }
+
+  @override
+  Future<void> close() {
+    // TODO: limpiar ServiceStatus Stream
+    return super.close();
   }
 }
